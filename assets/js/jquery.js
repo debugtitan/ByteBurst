b8300fe37e35d03e214fd3eb6ca74d9b6bca8a4a
debugtitan <alts.devs@gmail.com>
2024-01-10 22:02:20 +0000

    finding error why it's not working on smaller screens

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,13 +1,45 @@
-$(document).ready(()=>{
-    //NAVBAR
-    
-    $('#explore').click(() => {
-        $(".navbar").css('width', '100%')
-        $("#toggle").hide()
-        $(".logo").hide()
-    })
+$(document).ready(() => {
+    // NAVBAR
+
+    $("#explore").click(() => {
+        if ($(window).width()) {
+            $(".navbar").css({
+                "height": "100%",
+                "width": "100%",
+                "position": "fixed",
+                "z-index": "1",
+                "left": "0",
+                "top": "0",
+                "background-color": "var(--primary)",
+                "overflow-x": "hidden",
+                "transition": "0.5s",
+            });
+
+            $(".nav-items").css({
+                "position": "relative",
+                "top": "25%",
+                "width": "100%",
+                "text-align": "center",
+                "margin-top": "0 auto",
+            });
+
+
+            // Anchor styles
+            $(".navbar a").css({
+                "padding": "12px",
+                "font-size": "28px",
+                "text-decoration": "none",
+                "display": "block",
+                "color": "blue"
+                
+            });
+
+            $("#toggle").hide();
+            $(".logo").hide();
+        }
+    });
 
     $("#toggle-close").click(() => {
-        $(".navbar").css('width', '0')
-    })
-})
+        $(".navbar").css("width", "100%");
+    });
+});
